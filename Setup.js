bd7e67d2329fadb272cdc27af968188a6e50a2be
Henrique Peluzio <hpeluzio@gmail.com>
2021-05-07 16:45:47 -0300

    One Signal push notification implemented

--- Setup.js
+++ Setup.js
@@ -7,6 +7,11 @@ import database from '@react-native-firebase/database'
 import firestore from '@react-native-firebase/firestore'
 import storage from '@react-native-firebase/storage'
 
+import OneSignal from 'react-native-onesignal'
+
+// React.useEffect(() => {
+//   console.log('OneSignal')
+// }, [])
 // const firebaseConfig = {
 //   apiKey: 'AIzaSyAYaj2U78klybNpw-Sd053kgGlN7tbV3SI',
 //   authDomain: 'react-native-firebase-6ee91.firebaseapp.com',
@@ -24,6 +29,20 @@ import storage from '@react-native-firebase/storage'
 export { firebase, Auth, database, firestore, storage }
 
 const Setup = () => {
+  React.useEffect(() => {
+    OneSignal.setAppId('cfb3f8f7-1d1e-4c86-bd39-000f9bba1473')
+    // OneSignal.init('cfb3f8f7-1d1e-4c86-bd39-000f9bba1473')
+    // console.log('OneSignal')
+    OneSignal.addSubscriptionObserver((event) => {
+      console.log('OneSignal: subscription changed:', event)
+    })
+  }, [])
+
+  const onOpened = (result) => {
+    console.log('Mensagem: ', result.notification.payload.body)
+    console.log('result: ', result)
+  }
+
   return <App />
 }
 
