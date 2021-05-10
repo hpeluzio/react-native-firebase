7c97b795fd78a124b5e7c7840fe60c6aec4374bf
Henrique Peluzio <hpeluzio@gmail.com>
2021-05-10 12:39:12 -0300

    One Signal push notification 3.9.3 implemented

--- Setup.js
+++ Setup.js
@@ -30,26 +30,40 @@ export { firebase, Auth, database, firestore, storage }
 
 const Setup = () => {
   React.useEffect(() => {
-    OneSignal.setAppId('cfb3f8f7-1d1e-4c86-bd39-000f9bba1473')
-    // OneSignal.init('cfb3f8f7-1d1e-4c86-bd39-000f9bba1473')
-    console.log('OneSignal')
-    // OneSignal.addSubscriptionObserver((event) => {
-    //   console.log('OneSignal: subscription changed:', event)
+    // OneSignal.setAppId('cfb3f8f7-1d1e-4c86-bd39-000f9bba1473')
+    OneSignal.init('cfb3f8f7-1d1e-4c86-bd39-000f9bba1473')
+    OneSignal.addEventListener('received', onReceived)
+    OneSignal.addEventListener('opened', onOpened)
+    OneSignal.addEventListener('ids', onIds)
+
+    return () => {
+      OneSignal.removeEventListener('received', onReceived)
+      OneSignal.removeEventListener('opened', onOpened)
+      OneSignal.removeEventListener('ids', onIds)
+    }
+
+    // console.log('OneSignal')
+    // OneSignal.setNotificationOpenedHandler((openedEvent) => {
+    //   console.log('OneSignal: notification opened:', openedEvent)
+    //   const { action, notification } = openedEvent
+    //   console.log('action:', action)
+    //   console.log('notification:', notification)
     // })
-    // OneSignal.setInAppMessageClickHandler((event) => {
-    //   console.log('OneSignal IAM clicked:', event)
-    // })
-    OneSignal.setNotificationOpenedHandler((openedEvent) => {
-      console.log('OneSignal: notification opened:', openedEvent)
-      const { action, notification } = openedEvent
-      console.log('action:', action)
-      console.log('notification:', notification)
-    })
   }, [])
 
   const onOpened = (result) => {
-    console.log('Mensagem: ', result.notification.payload.body)
-    console.log('result: ', result)
+    // console.log('onOpened Mensagem: ', result.notification.payload.body)
+    console.log('onOpened result: ', result)
+  }
+
+  const onReceived = (result) => {
+    // console.log('onReceived Mensagem: ', result.notification.payload.body)
+    console.log('onReceived result: ', result)
+  }
+
+  const onIds = (device) => {
+    // console.log('onReceived Mensagem: ', result.notification.payload.body)
+    console.log('device info: ', device)
   }
 
   return <App />
