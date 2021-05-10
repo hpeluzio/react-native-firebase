3c0fe9d5c06c3c8b8a4dfde74cce6ae6e08c6f14
Henrique Peluzio <hpeluzio@gmail.com>
2021-05-10 17:19:37 -0300

    Cloud Messaging implemented

--- Setup.js
+++ Setup.js
@@ -1,70 +1,102 @@
 import * as React from 'react'
 import App from './App'
 
+import { Alert } from 'react-native'
+
 import firebase from '@react-native-firebase/app'
 import Auth from '@react-native-firebase/auth'
 import database from '@react-native-firebase/database'
 import firestore from '@react-native-firebase/firestore'
 import storage from '@react-native-firebase/storage'
+import messaging from '@react-native-firebase/messaging'
 
 import OneSignal from 'react-native-onesignal'
 
-// React.useEffect(() => {
-//   console.log('OneSignal')
-// }, [])
-// const firebaseConfig = {
-//   apiKey: 'AIzaSyAYaj2U78klybNpw-Sd053kgGlN7tbV3SI',
-//   authDomain: 'react-native-firebase-6ee91.firebaseapp.com',
-//   databaseURL: 'https://rnfirebase-b9236-default-rtdb.firebaseio.com/',
-//   projectId: 'react-native-firebase-6ee91',
-//   storageBucket: 'react-native-firebase-6ee91.appspot.com',
-//   messagingSenderId: '409985603109',
-//   appId: '1:409985603109:web:15ec9e63a61d1c40e111cc',
-// }
-
-// if (!firebase.apps.length) {
-//   firebase.initializeApp(firebaseConfig)
-// }
-
-export { firebase, Auth, database, firestore, storage }
+export { firebase, Auth, database, firestore, storage, messaging }
 
 const Setup = () => {
+  //Receiving message
+  // https://rnfirebase.io/messaging/
+  // React.useEffect(() => {
+  //   const unsubscribe = messaging().onMessage(async (remoteMessage) => {
+  //     Alert.alert('A new FCM message arrived!', JSON.stringify(remoteMessage))
+  //   })
+
+  //   return unsubscribe
+  // }, [])
+
+  // https://rnfirebase.io/messaging/notifications#handling-interaction
   React.useEffect(() => {
-    // OneSignal.setAppId('cfb3f8f7-1d1e-4c86-bd39-000f9bba1473')
-    OneSignal.init('cfb3f8f7-1d1e-4c86-bd39-000f9bba1473')
-    OneSignal.addEventListener('received', onReceived)
-    OneSignal.addEventListener('opened', onOpened)
-    OneSignal.addEventListener('ids', onIds)
+    setupCloudMessaging()
 
-    return () => {
-      OneSignal.removeEventListener('received', onReceived)
-      OneSignal.removeEventListener('opened', onOpened)
-      OneSignal.removeEventListener('ids', onIds)
-    }
+    // Quando o aplicativo está em execução, mas em segundo plano.
+    messaging().onNotificationOpenedApp((remoteMessage) => {
+      alert('onNotificationOpenedApp', JSON.stringify(remoteMessage))
+    })
 
-    // console.log('OneSignal')
-    // OneSignal.setNotificationOpenedHandler((openedEvent) => {
-    //   console.log('OneSignal: notification opened:', openedEvent)
-    //   const { action, notification } = openedEvent
-    //   console.log('action:', action)
-    //   console.log('notification:', notification)
-    // })
+    // Quando o aplicativo é aberto a partir de um estado de encerramento.
+    messaging().getInitialNotification((remoteMessage) => {
+      alert('getInitialNotification', JSON.stringify(remoteMessage))
+    })
+
+    // Para ouvir as mensagens em primeiro plano, chame o onMessage
+    // dentro do código do seu aplicativo.
+    messaging().onMessage((remoteMessage) => {
+      Alert.alert('onMessage', JSON.stringify(remoteMessage))
+    })
   }, [])
 
-  const onOpened = (result) => {
-    // console.log('onOpened Mensagem: ', result.notification.payload.body)
-    console.log('onOpened result: ', result)
+  const setupCloudMessaging = async () => {
+    //getToken
+    const token = await messaging().getToken()
+    alert(token)
+
+    const authStatus = await messaging().requestPermission()
+    const enabled =
+      authStatus === messaging.AuthorizationStatus.AUTHORIZED ||
+      authStatus === messaging.AuthorizationStatus.PROVISIONAL
+
+    if (enabled) {
+      console.log('Authorization status:', authStatus)
+    }
   }
 
-  const onReceived = (result) => {
-    // console.log('onReceived Mensagem: ', result.notification.payload.body)
-    console.log('onReceived result: ', result)
-  }
+  // React.useEffect(() => {
+  //   // OneSignal.setAppId('cfb3f8f7-1d1e-4c86-bd39-000f9bba1473')
+  //   OneSignal.init('cfb3f8f7-1d1e-4c86-bd39-000f9bba1473')
+  //   OneSignal.addEventListener('received', onReceived)
+  //   OneSignal.addEventListener('opened', onOpened)
+  //   OneSignal.addEventListener('ids', onIds)
 
-  const onIds = (device) => {
-    // console.log('onReceived Mensagem: ', result.notification.payload.body)
-    console.log('device info: ', device)
-  }
+  //   return () => {
+  //     OneSignal.removeEventListener('received', onReceived)
+  //     OneSignal.removeEventListener('opened', onOpened)
+  //     OneSignal.removeEventListener('ids', onIds)
+  //   }
+
+  //   // console.log('OneSignal')
+  //   // OneSignal.setNotificationOpenedHandler((openedEvent) => {
+  //   //   console.log('OneSignal: notification opened:', openedEvent)
+  //   //   const { action, notification } = openedEvent
+  //   //   console.log('action:', action)
+  //   //   console.log('notification:', notification)
+  //   // })
+  // }, [])
+
+  // const onOpened = (result) => {
+  //   // console.log('onOpened Mensagem: ', result.notification.payload.body)
+  //   console.log('onOpened result: ', result)
+  // }
+
+  // const onReceived = (result) => {
+  //   // console.log('onReceived Mensagem: ', result.notification.payload.body)
+  //   console.log('onReceived result: ', result)
+  // }
+
+  // const onIds = (device) => {
+  //   // console.log('onReceived Mensagem: ', result.notification.payload.body)
+  //   console.log('device info: ', device)
+  // }
 
   return <App />
 }
--- index.js
+++ index.js
@@ -6,4 +6,10 @@ import { AppRegistry } from 'react-native'
 import Setup from './Setup'
 import { name as appName } from './app.json'
 
+import messaging from '@react-native-firebase/messaging'
+
+messaging().setBackgroundMessageHandler(async (remoteMessage) => {
+  console.log('Message handled in the background!', remoteMessage)
+})
+
 AppRegistry.registerComponent(appName, () => Setup)
